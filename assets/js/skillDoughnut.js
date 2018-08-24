03f63c360079a99d8e98e6983a2262fc4e6276a4
Amarnath Navuluri <Navuluri.amarnath@oracle.com>
2018-08-24 08:06:24 +0530

    Updated the changes for the Doughnut Java script

--- assets/js/skillDoughnut.js
+++ assets/js/skillDoughnut.js
@@ -4,16 +4,34 @@ var id = document.currentScript.getAttribute('id'); //1
 
 var doughnutData = [
 									{
-										value:(function(){ 
-        var myvariable = known; 
-        return myvariable; 
-    })(),
+										value: parseInt(known),
 										color:"#1abc9c"
 									},
 									{
-										value : 20,
+										value : parseInt(unknown),
 										color : "#ecf0f1"
 									}
 								];
-var myDoughnut = new Chart(document.getElementById('db').getContext("2d")).Doughnut(doughnutData);
-						
+var myDoughnut = new Chart(document.getElementById(id).getContext("2d")).Doughnut(doughnutData);
+var inView = false;
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+}
+
+$(window).scroll(function() {
+    if (isScrolledIntoView('#canvas')) {
+        if (inView) { return; }
+        inView = true;
+        new Chart(document.getElementById(id).getContext("2d")).Pie(data);
+    } else {
+        inView = false;
+    }
+});
